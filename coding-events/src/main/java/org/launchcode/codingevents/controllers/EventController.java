56700eb6fdb9c3deb5e3a96bd0d15678b87a6c2e
Hope Ciuffa <hopeciuffa@gmail.com>
2021-10-09 15:20:21 -0500

    create event

--- coding-events/src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ coding-events/src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -4,27 +4,37 @@ package org.launchcode.codingevents.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 @Controller
 @RequestMapping("events")
 public class EventController {
+    private static List<String> events = new ArrayList<>();
 
     @GetMapping
     public String displayAllEvents(Model model){
-        List<String> events = new ArrayList<>();
-        events.add("Code with Pride");
-        events.add("Strange Loop");
-        events.add("Apple wwDC");
-        events.add("SpringOne Platform");
+        HashMap<String, String> events = new HashMap();
+            events.put("Hour of Code", "Getting young minds interested in coding");
+            events.put("Strange Loop", "Coding conference of professionals sharing ideas");
+            events.put("CES", "Annual trade show showing awesome new tech");
         model.addAttribute("events", events);
         return "events/index";
     }
+
     @GetMapping("create")
-    public String renderCreateEventForm(){
+    public String renderCreateEventForm(Model model){
         return "events/create";
+    }
+
+    @PostMapping("create")
+    public String createEvent(@RequestParam String eventName){
+       events.add(eventName);
+       return "redirect:";
     }
 }
